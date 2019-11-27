4194d2f7a1ddbf0a94936f4bf72b5e922838ca40
Simon <simonaxe11@hotmail.com>
2019-11-27 13:43:07 +0100

    Maven y eclipse

---
+++ 07_MVN_ModeloUsuario/src/main/java/com/vn/appusuarios/modelo/Usuario.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vn.appusuarios.modelo;
+
+/**
+ *
+ * @author pc
+ */
+public class Usuario {
+
+    private int id;
+    private String email;
+    private String password;
+    private String nombre;
+    private int edad;
+
+    public Usuario(int id, String email, String password, String nombre, int edad) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (super.equals(obj)) {
+            return true;
+        } else {
+            if (obj == null) {
+                return false;
+            }
+            Usuario usu = (Usuario) obj;
+            return usu.getId() == this.getId() && (usu.getEmail() == null ? this.getEmail() == null
+                    : usu.getEmail().equals(this.getEmail()));
+
+        }
+    }
+
+    /**
+     * Get the value of edad
+     *
+     * @return the value of edad
+     */
+    public int getEdad() {
+        return edad;
+    }
+
+    /**
+     * Set the value of edad
+     *
+     * @param edad new value of edad
+     */
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    /**
+     * Get the value of nombre
+     *
+     * @return the value of nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Set the value of nombre
+     *
+     * @param nombre new value of nombre
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
---
+++ 07_MVN_ModeloUsuario/src/main/java/com/vn/appusuarios/modelo/logica/ServicioUsuarios.java
@@ -0,0 +1,187 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vn.appusuarios.modelo.logica;
+
+import com.vn.appusuarios.modelo.Usuario;
+import com.vn.appusuarios.modelo.dao.UsuariosDAO;
+import com.vn.appusuarios.modelo.logica.ChivatoServicios;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author pc
+ */
+public class ServicioUsuarios {
+    
+    UsuariosDAO daoUsu;
+    ChivatoServicios chivato;
+    
+    public ServicioUsuarios() {
+        daoUsu = new UsuariosDAO();
+    }
+
+    public void setChivatoListener(ChivatoServicios chivato) {
+        this.chivato = chivato;
+    }
+
+    /**
+     * Método que comprueba primero si los campos son válidos de la siguiente
+     * manera: Primero que ningún valor es nulo, segundo que no haya campos
+     * vacíos. La edad debe ser un número y por último que el email y el nombre
+     * sean correctos mediante expresiones regulares.
+     *
+     * @param email
+     * @param password
+     * @param nombre
+     * @param edad Debe ser mayor de 12
+     * @return El objeto Usuario ya creado o nulo si está mal
+     */
+    public Usuario crearUsuarioValido(String email, String password, String nombre, String edad) {
+        
+        if (email != null
+                && password != null
+                && nombre != null
+                && edad != null) {
+            if (email.length() >= 3
+                    && nombre.length() > 1
+                    && password.length() >= 4
+                    && !"".equals(edad)) {
+                try {
+                    int iEdad = Integer.parseInt(edad);
+                    if (iEdad >= 12) {
+                        if (validarEmailNombre(email, nombre)) {                            
+                            Usuario usuario = new Usuario(-1, email, password, nombre, iEdad);
+                            return usuario;
+                        } else {
+                            notificarError("crearUsuarioValido: El nombre o el email no cumplen RegExp: "
+                                    + nombre + ", " + password);
+                        }
+                    } else {
+                        notificarError("crearUsuarioValido: La edad es menor de 12: "
+                                + edad);
+                    }
+                } catch (NumberFormatException e) {
+                    notificarError("crearUsuarioValido: La edad no se puede parsear a int: " + edad
+                            + " error: " + e.getMessage());
+                } catch (Exception e) {
+                    notificarError("crearUsuarioValido: Error al crear el objeto usuario: " + e.getMessage());
+                }
+            } else {
+                notificarError("crearUsuarioValido: Hay un campo que no cumple la longitud");
+            }
+        } else {
+            notificarError("crearUsuarioValido: No admite NULOS");
+        }
+        return null;
+    }
+
+    void notificarError(String mensajeError) {
+        
+        System.out.println("Error ServicioUsuario:  " + mensajeError);
+        if (chivato != null) {
+            chivato.notificarError(mensajeError);
+        }
+    }
+
+    boolean validarEmailNombre(String email, String nombre) {
+        String regex = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^-]+(?:\\.[a-zA-Z0-9_!#$%&'*+/=?`{|}~^-]+)*@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+        if (matcher.find()) {
+            regex = "^[A-Z][a-z]+ ?[A-Za-z]*$";
+            pattern = Pattern.compile(regex);
+            matcher = pattern.matcher(nombre);
+            
+            if (matcher.find()) {
+                return true;
+            } else {
+                notificarError("El nombre no cumple Expr Reg. ");
+            }
+        } else {
+            notificarError("El email no cumple Expr Reg. ");
+        }
+        return false;
+    }
+    
+    public Usuario crear(String email, String password, String nombre, String edad) {
+        Usuario nuevoUsu = crearUsuarioValido(email, password, nombre, edad);
+        if (nuevoUsu != null) {
+            try {
+                nuevoUsu = daoUsu.crear(nuevoUsu);
+            } catch (ClassNotFoundException ex) {
+                notificarError("crear: No se ha encontrado el driver: " + ex.getMessage());
+            } catch (SQLException ex) {
+                notificarError("crear: Error en bb.dd.: " + ex.getMessage());
+            } catch (Exception ex) {
+                Logger.getLogger(ServicioUsuarios.class.getName()).log(Level.SEVERE, null, ex);
+                notificarError(ex.getMessage());
+            }
+            return nuevoUsu;
+        }
+        return null;
+    }
+    
+    public Usuario leerUno(int id) {
+        try {
+            return daoUsu.obtenerPorId(id);
+        } catch (Exception ex) {
+            Logger.getLogger(ServicioUsuarios.class.getName()).log(Level.SEVERE, null, ex);
+            notificarError("Leer uno: " + ex.getMessage());
+        }
+        return null;
+    }
+    
+    public Usuario leerUno(String email) {
+        try {
+            return daoUsu.obtenerPorEmail(email);
+        } catch (Exception ex) {
+            Logger.getLogger(ServicioUsuarios.class.getName()).log(Level.SEVERE, null, ex);
+            notificarError("Leer uno: " + ex.getMessage());
+        } 
+        return null;
+    }
+    
+    public Usuario modificar(int id, String email, String password, String nombre, String edad) {
+        
+        Usuario usuModif = crearUsuarioValido(email, password, nombre, edad);
+        if (usuModif != null) {
+            usuModif.setId(id);
+            try {
+                daoUsu.modificar(usuModif);
+            } catch (Exception ex) {
+                Logger.getLogger(ServicioUsuarios.class.getName()).log(Level.SEVERE, null, ex);
+                notificarError("modificar: " + ex.getMessage());
+            }
+            return usuModif;
+        }
+        return null;
+    }
+    
+    public boolean eliminar(int id) {
+        try {
+            return daoUsu.eliminar(id);
+        } catch (Exception ex) {
+            Logger.getLogger(ServicioUsuarios.class.getName()).log(Level.SEVERE, null, ex);
+            notificarError("eliminar: " + ex.getMessage());
+        }
+        return false;
+    }
+
+    public ArrayList<Usuario> leerTodos() {
+        try {
+            return daoUsu.obtenerTodos();
+        } catch (Exception ex) {
+            Logger.getLogger(ServicioUsuarios.class.getName()).log(Level.SEVERE, null, ex);
+            notificarError("leerTodos: " + ex.getMessage());
+        }
+        return null;
+    }
+}
